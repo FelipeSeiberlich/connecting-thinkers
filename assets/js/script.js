678f89fed5bf1a3ddd641feb23172a5c0982e68a
Felipe Seiberlich <philzaiber@outlook.com>
2023-05-19 13:03:12 +0000

    Add code to display current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,5 +25,33 @@ function printDay(){
 }
 printDay()
 
+function printTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var period = "AM";
+    if(hour == 0){
+        hour = 12;
+    }
+    if(hour > 12){
+        hour = hour - 12; // if hour is 13 ===> hour = 13  - 12 = 1
+        period = "PM";
+    }
+    if(hour < 10){
+        hour = "0" + hour;
+    }
+    if(min < 10){
+        min = "0" + min;
+    }
+    if(sec < 10){
+        sec = "0" + sec;
+    }
+    var time = hour + " : " + min + " : " + sec + " " + period;
+    document.querySelector(".time").innerHTML = time;
+    setTimeout(printTime,1000);
+}
+printTime()
+
 //Quiz game
 
