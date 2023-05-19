61c79562f83494806260133c71681c0d54a99cdb
Felipe Seiberlich <philzaiber@outlook.com>
2023-05-19 12:15:56 +0000

    Add code to display today's date in the History section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,20 @@
 const toTop = () => window.scrollTo({top:0, behavior: "smooth"});
 
 // Date, day & time
+var date = new Date();
+const todaydate = document.querySelector(".date");
+const todayday = document.querySelector(".day");
+const todaytime = document.querySelector(".time");
+const noofday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function printDate(){
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth()+1;
+    var year = date.getFullYear();
+    todaydate.innerHTML = day + "/" + month + "/" + year;
+}
+printDate()
 
 //Quiz game
 
