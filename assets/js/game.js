3da0c2fad1bf0cd2fa95598b908d42e17123a02f
Felipe Seiberlich <philzaiber@outlook.com>
2023-05-22 22:19:48 +0000

    Add code to display correct and wrong answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -45,7 +45,27 @@ function resetState() {
 }
 
 function selectAnswer() {
+    const selectButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answer,answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove('hide')
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    }   else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 } 
 
 const questions = [
