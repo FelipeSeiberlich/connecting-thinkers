830d253f31aeeee2bf428b74c047651a531bd1b0
Felipe Seiberlich <philzaiber@outlook.com>
2023-05-22 09:31:36 +0000

    Add code to shuffle the cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,6 @@
 const startButton = document.getElementById('start-btn')
 const questionCardElement = document.getElementById('quiz-question-container')
+const shuffledCardQuestions, currentCardquestionIndex
 
 startButton.addEventListener('click', startQuiz)
 
@@ -8,6 +9,8 @@ function startQuiz() {
     startButton.classList.add('hide')
     questionCardElement.classList.remove('hide')
     setNextCardQuestion()
+    shuffledCardQuestions = questions.sort(() => Math.random() - .6)
+    currentCardquestionIndex = 0
 }
 
 function setNextCard() {
@@ -17,3 +20,15 @@ function setNextCard() {
 function selectAnswer() {
 
 } 
+
+const questions [
+    {
+        question: 'How many basic operations is the brain able to perform per second?',
+        answer: [
+            {text: '1000' correct: true },
+            {text: '10' correct: false },
+            {text: '100'},
+            {text: '500'},
+        ]
+    }
+]
