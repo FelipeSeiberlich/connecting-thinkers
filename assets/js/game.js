1357abe38432754d9f734392a6e625222157a3dd
Felipe Seiberlich <philzaiber@outlook.com>
2023-05-24 17:24:16 +0000

    Add new code in the JS file with errors fixed and changing body padding

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,117 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('quiz-question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startQuiz() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn-quiz')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'The first alarm clock only rings at?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+  {
+    question: 'What was Nokia s first product?',
+    answers: [
+      { text: 'Web Dev Simplified', correct: true },
+      { text: 'Traversy Media', correct: true },
+      { text: 'Dev Ed', correct: true },
+      { text: 'Fun Fun Function', correct: true }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'The world s first computer mouse was made of?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
+]
+/*const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const quizQuestionContainerElement = document.getElementById
 ('quiz-question-container')
 const questionElement = document.getElementById('question')
@@ -80,10 +192,37 @@ function clearStatusClass(element) {
 
 const questions = [
     {
-        question: 'I am testing something !',
-        answer: [
-            { text: 'miaow', correct: true },
-            { text: 'moremiaow', correct: false }
-        ]
+      question: 'The first alarm clock only rings at?',
+      answers: [
+        { text: 'a', correct: true },
+        { text: 'b', correct: false },
+        { text: 'c', correct: true },
+        { text: 'd', correct: true }
+      ]
+    },
+    {
+      question: 'What was Nokia s first product?',
+      answers: [
+        { text: 'a', correct: true },
+        { text: 'b', correct: true },
+        { text: 'c', correct: true },
+        { text: 'd', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'a', correct: false },
+        { text: 'b!!!', correct: true },
+        { text: 'c', correct: false },
+        { text: 'd', correct: false }
+      ]
+    },
+    {
+      question: 'The world s first computer mouse was made of?',
+      answers: [
+        { text: 'a', correct: false },
+        { text: 'b', correct: true }
+      ]
     }
-]
+  ]*/
